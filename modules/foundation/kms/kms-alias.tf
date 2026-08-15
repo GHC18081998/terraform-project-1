cb8e08b07253c459199d1db3f25dcac53c7b9082
Chandu <106138199+GHC18081998@users.noreply.github.com>
2026-08-15 10:46:27 +0530

    Delete modules directory

--- modules/foundation/kms/kms-alias.tf
+++
@@ -1,8 +0,0 @@
-# ==============================================================
-# KMS Alias Resources
-# ==============================================================
-
-resource "aws_kms_alias" "this" {
-  name          = "alias/${local.name_prefix}-${var.key_purpose}"
-  target_key_id = aws_kms_key.this.key_id
-}
